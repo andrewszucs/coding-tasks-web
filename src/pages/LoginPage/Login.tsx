f60c3151089913d574f1d49cdb2d32cc45a8a61f
Andras Szucs <andrew0szucs@gmail.com>
2019-11-09 14:50:49 +0100

    Add show password functionality to Login form

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "@reach/router";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { Button, TextField } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  InputAdornment,
+  IconButton
+} from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 
 // TODO: theme spacing
 const useStyles = makeStyles({
@@ -20,6 +26,7 @@ const useStyles = makeStyles({
 export default function LoginPage(props: RouteComponentProps) {
   // TODO: when API req comes back it sets the token in the state
   // if token is set already, Login should render a Redirect to /
+  const [showPassword, setShowPassword] = useState(false);
 
   const classes = useStyles();
   const formik = useFormik({
@@ -52,12 +59,25 @@ export default function LoginPage(props: RouteComponentProps) {
       <TextField
         id="password"
         label="Password"
-        type="password"
+        type={showPassword ? "text" : "password"}
         autoComplete="current-password"
         margin="normal"
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
         value={formik.values.password}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(showPassword ? false : true)}
+                onMouseDown={event => event.preventDefault()}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
       />
       <Button variant="contained" color="primary" type="submit">
         Login
