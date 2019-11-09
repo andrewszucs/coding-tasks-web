8d694a7ac765a83e77751f1fcc49a336195398c7
Andras Szucs <andrew0szucs@gmail.com>
2019-11-09 01:04:22 +0100

    Add ExchangePage component with placeholder

---
+++ src/pages/ExchangePage/ExchangePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+// TODO: Add components and styling
+export default function ExchangePage(props: RouteComponentProps) {
+  return <div>ExchangePage</div>;
+}
