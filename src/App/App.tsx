ba570f3a32ef69f1e6cab19ffa8e4b33a86ed8ed
Andras Szucs <andrew0szucs@gmail.com>
2019-11-09 01:05:10 +0100

    Implement Routing in App with Page components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Router } from "@reach/router";
+import LoginPage from "../pages/LoginPage";
+import ExchangePage from "../pages/ExchangePage";
+import ExchangeFlowPage from "../pages/ExchangeFlowPage";
 
+import { CssBaseline } from "@material-ui/core";
+
+// TODO: load token from localStorage as the initialState
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Router>
+        <LoginPage path="/login" />
+        <ExchangePage path="/" />
+        <ExchangeFlowPage path="/exchange" />
+      </Router>
+    </>
   );
 };
 
