4a7568d387920f10d9f8104e03ae41ba693e03a5
Andras Szucs <andrew0szucs@gmail.com>
2019-11-10 21:01:30 +0100

    Move App back to root

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Router } from "@reach/router";
-import LoginPage from "../pages/LoginPage";
-import ExchangePage from "../pages/ExchangePage";
-import ExchangeFlowPage from "../pages/ExchangeFlowPage";
+import LoginPage from "./pages/LoginPage";
+import ExchangePage from "./pages/ExchangePage";
+import ExchangeFlowPage from "./pages/ExchangeFlowPage";
 
 import { CssBaseline } from "@material-ui/core";
+import { Provider } from "react-redux";
+
+import store from "./store";
 
 // TODO: load token from localStorage as the initialState
 const App: React.FC = () => {
   return (
-    <>
+    <Provider store={store}>
       <CssBaseline />
       <Router>
         <LoginPage path="/login" />
         <ExchangePage path="/" />
         <ExchangeFlowPage path="/exchange" />
       </Router>
-    </>
+    </Provider>
   );
 };
 
