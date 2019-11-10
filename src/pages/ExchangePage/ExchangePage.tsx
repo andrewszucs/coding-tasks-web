ea0904a749c53eef3ee506d4334c910c5351186c
Andras Szucs <andrew0szucs@gmail.com>
2019-11-10 21:04:30 +0100

    Implement fetchCountries and fetchCountry in ExchangePage

--- src/pages/ExchangePage/ExchangePage.tsx
+++ src/pages/ExchangePage/ExchangePage.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import { RouteComponentProps, Link } from "@reach/router";
-import Select from "react-select";
-import { Typography, Button, TextField } from "@material-ui/core";
+import Select, { OptionsType, ValueType, OptionTypeBase } from "react-select";
+import {
+  Typography,
+  Button,
+  TextField,
+  Card,
+  CardContent,
+  CardActionArea
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import numbro from "numbro";
+import { Person } from "@material-ui/icons";
+import { connect } from "react-redux";
+import {
+  FetchCountriesRequestType,
+  FetchCountryDetailRequestType,
+  fetchCountriesRequest,
+  fetchCountryDetailRequest
+} from "../../store/actions";
+import { CountrySuggestionsState, CountryShortlistState } from "../../types";
+import { AppState } from "../../store/reducers";
 
 const useStyles = makeStyles({
   root: {
@@ -36,18 +54,94 @@ const useStyles = makeStyles({
   },
   exchangeInput: {
     width: "33%"
+  },
+  countryList: {
+    overflow: "scroll",
+    margin: "10px 0"
+  },
+  listGrid: {
+    display: "grid",
+    gridTemplateColumns: "48% 48%",
+    gridGap: "4%"
+  },
+  flagCircle: {
+    height: "3rem",
+    width: "3rem",
+    borderRadius: "50%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundColor: "#FFFFFF",
+    border: "1px solid #e0e0e0"
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  populationIcon: { marginRight: "0.1rem" },
+  populationContainer: { display: "flex", alignItems: "flex-end" },
+  currencyList: {
+    padding: 0,
+    marginBottom: 0
+  },
+  currencyRow: {
+    listStyle: "none"
+  },
+  currencyName: {
+    fontSize: "0.75rem"
+  },
+  currencyValue: {
+    textAlign: "center"
   }
 });
 
+interface Props
+  extends RouteComponentProps,
+    CountrySuggestionsState,
+    CountryShortlistState {
+  fetchCountries: FetchCountriesRequestType;
+  fetchCountry: FetchCountryDetailRequestType;
+}
+
 // TODO: Add components and styling
-export default function ExchangePage(props: RouteComponentProps) {
+function ExchangePage({
+  fetchCountries,
+  fetchCountry,
+  countrySuggestions,
+  isLoadingCountrySuggestions,
+  countryShortlist,
+  isLoadingCountryDetail
+}: Props) {
   const classes = useStyles();
+
+  const countries = countryShortlist.map(
+    ({ alpha3Code: id, name, flag, population, currencies }) => ({
+      id,
+      name,
+      flag,
+      population: numbro(population).format({ average: true, mantissa: 2 }),
+      currencies: currencies.map(({ code, name, symbol, rate }) => ({
+        name,
+        code,
+        symbol,
+        value: rate
+          ? (290 * rate).toLocaleString("en-US", {
+              maximumFractionDigits: 0
+            })
+          : "No data"
+      }))
+    })
+  );
+
   return (
     <div className={classes.root}>
       <Typography variant="h5" className={`${classes.center}`}>
         Send
       </Typography>
-      <div className={`${classes.center} ${classes.exchangeInputContainer}`}>
+      <section
+        className={`${classes.center} ${classes.exchangeInputContainer}`}
+      >
         <div dir="rtl" className={classes.exchangeInput}>
           <TextField
             placeholder="0"
@@ -62,25 +156,34 @@ export default function ExchangePage(props: RouteComponentProps) {
         <Typography variant="h6" className={`${classes.verticalCenter}`}>
           SEK
         </Typography>
-      </div>
+      </section>
       <Typography variant="h5" className={`${classes.center}`}>
         To
       </Typography>
       <Select
         name="country"
-        isLoading={false}
+        isLoading={isLoadingCountrySuggestions}
         isClearable
         isSearchable
-        placeholder="Type the name of a country"
-        options={[{ value: 1, label: "1" }]}
+        placeholder="Type a country's name"
+        options={countrySuggestions.map(({ alpha3Code, name }) => ({
+          value: alpha3Code,
+          label: name
+        }))}
         value={null}
-        onChange={(o, { action }) => {
-          if (action !== "select-option") return;
-          console.log("change", o, action);
+        onChange={(clickedItem, { action }) => {
+          if (
+            action !== "select-option" ||
+            !clickedItem ||
+            Array.isArray(clickedItem)
+          ) {
+            return;
+          }
+          fetchCountry((clickedItem as OptionTypeBase).value);
         }}
         onInputChange={(inputValue, { action }) => {
-          if (action !== "input-change") return;
-          console.log("input", inputValue);
+          if (action !== "input-change" || inputValue.length <= 0) return;
+          fetchCountries(inputValue);
         }}
         noOptionsMessage={({ inputValue }) => {
           if (inputValue !== "") {
@@ -89,7 +192,44 @@ export default function ExchangePage(props: RouteComponentProps) {
           return "Start typing to search!";
         }}
       />
-      <div className={classes.stretch}>Country shortlist</div>
+      <section className={`${classes.stretch} ${classes.countryList}`}>
+        <div className={classes.listGrid}>
+          {countries.map(({ id, name, flag, population, currencies }) => (
+            <Card key={id}>
+              <CardActionArea>
+                <CardContent className={classes.cardContent}>
+                  <div
+                    style={{ backgroundImage: `url("${flag}")` }}
+                    className={classes.flagCircle}
+                  />
+                  <Typography variant="subtitle1">{name}</Typography>
+                  <Typography
+                    variant="caption"
+                    className={classes.populationContainer}
+                  >
+                    <Person className={classes.populationIcon} />
+                    {population}
+                  </Typography>
+                  <ul className={classes.currencyList}>
+                    {currencies.map(({ name, code, symbol, value }) => (
+                      <li className={classes.currencyRow} key={code}>
+                        <Typography
+                          variant="body1"
+                          className={classes.currencyName}
+                        >{`${name} (${code})`}</Typography>
+                        <Typography
+                          variant="h6"
+                          className={classes.currencyValue}
+                        >{`${value} ${symbol}`}</Typography>
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
+      </section>
       <Button
         variant="contained"
         color="primary"
@@ -101,3 +241,29 @@ export default function ExchangePage(props: RouteComponentProps) {
     </div>
   );
 }
+
+export default connect(
+  ({
+    countrySuggestions: {
+      countrySuggestions,
+      isLoadingCountrySuggestions,
+      errorCountrySuggestions
+    },
+    countryShortlist: {
+      countryShortlist,
+      isLoadingCountryDetail,
+      errorCountryDetail
+    }
+  }: AppState) => ({
+    countrySuggestions,
+    isLoadingCountrySuggestions,
+    errorCountrySuggestions,
+    countryShortlist,
+    isLoadingCountryDetail,
+    errorCountryDetail
+  }),
+  {
+    fetchCountries: fetchCountriesRequest,
+    fetchCountry: fetchCountryDetailRequest
+  }
+)(ExchangePage);
